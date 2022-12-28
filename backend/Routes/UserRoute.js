0c1b232a5caf3f4d1159f108fd90572ba65e9047
SARB <mrsinghbusiness05@gmail.com>
2022-12-28 15:39:10 +0530

    Account Deleted

--- backend/Routes/UserRoute.js
+++ backend/Routes/UserRoute.js
@@ -1,6 +1,6 @@
-const router =  require('express').Router();
-const User =    require('../Modules/Auth');
-const bcrypt =  require('bcrypt');
+const router =   require('express').Router();
+const User   =   require('../Modules/Auth');
+const bcrypt =   require('bcrypt');
 
 // update the user now Registered 
 router.put('/:id',  async (req,res) => {
@@ -25,12 +25,34 @@ router.put('/:id',  async (req,res) => {
             {
                 res.status(500).json(err);
             }
-        }else{
+        }else
+        {
                 res.status(401).json('You can only  update your ACCOUNT ');
         }
 })
 
-
 // delete that  user  also 
+router.delete('/:id' , async(req,res) => {
+
+      if(req.body.userid === req.params.id)
+      {
+        try{
+            const user = await User.findById(req.params.id);
+                try{
+                     await User.findByIdAndDelete(req.params.id);    
+                    res.status(200).json(' User has been Deleted ...... ');
+                    }catch(err)
+                    {
+                        res.status(500).json(err);
+                    }
+        }catch(err)
+        {
+            res.status(404).json(' User not Found !!!! ');
+        }
+      }
+      else{
+          res.status(401).json(' You can only delete your Acount !!!! ');
+       }
+})
 
 module.exports = router;
